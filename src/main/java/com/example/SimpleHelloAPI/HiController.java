250c5479c029c7a1b663ec4c4efa2ba964f1ad45
Damanjit Singh <damanjits@wolfram.com>
2023-04-23 15:52:53 -0500

    added deploy workflow

--- src/main/java/com/example/SimpleHelloAPI/HiController.java
+++ src/main/java/com/example/SimpleHelloAPI/HiController.java
@@ -3,14 +3,11 @@ package com.example.SimpleHelloAPI;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HiController {
 
-    @RestController
-    public class HelloController {
-
-        @GetMapping("/hiapi")
-        public String handleGetMethod () {
-            return "Hi, Welcome to the demo Hello API from Spring.";
-        }
+    @GetMapping("/hiapi")
+    public String handleGetMethod () {
+        return "Hi, Welcome to the demo Hello API from Spring.";
     }
 }
